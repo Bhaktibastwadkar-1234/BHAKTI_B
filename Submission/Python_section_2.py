242416c744980ebc082bb1d41b974f26abaf74a8
Bhaktibastwadkar-1234 <105408440+Bhaktibastwadkar-1234@users.noreply.github.com>
2024-10-23 19:45:46 +0530

    Update Python_section_2.py

--- Submission/Python_section_2.py
+++ Submission/Python_section_2.py
@@ -1,1 +1,25 @@
+#Question 9: Distance Matrix Calculation
+
+import pandas as pd
+import numpy as np
+
+def calculate_distance_matrix(file_path):
+    
+    df = pd.read_csv(file_path)
+    
+    
+    distance_matrix = pd.pivot_table(df, values='distance', 
+                                      index='from', 
+                                      columns='to', 
+                                      fill_value=0)
+
+    distance_matrix = distance_matrix.add(distance_matrix.T, fill_value=0)
+    
+   
+    np.fill_diagonal(distance_matrix.values, 0)
+    
+    distance_matrix = distance_matrix.fillna(0)
+    
+    return distance_matrix
+
 
